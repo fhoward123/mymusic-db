65223a73c6973341b4389c697fd02e4a6be46d0e
fhoward123 <42585731+fhoward123@users.noreply.github.com>
2019-06-15 22:23:47 -0500

    Added 'fetchAlbums' method to get album list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 const axios = require('axios');
-// import axios from 'axios';
+
 /// components ///
 // import Header from './components/Header'
 
@@ -10,37 +10,51 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            apiResponse: ""
+            submitter: '',
+            title: '',
+            artist: '',
+            image: '',
+            genre: '',
+            label: '',
+            trackCnt: 0,
+            runtime: '',
+            media: '',
+            yearMFG: 0,
+            yearReleased: 0,
+            barcode: 0,
+            songs: []
         }
-        // this.fetchItems = this.fetchItems.bind(this)
-        this.callAPI = this.callAPI.bind(this)
+        this.fetchAlbums = this.fetchAlbums.bind(this)
     }
 
-    callAPI() {
-        axios.get("https://mymusic-backend.herokuapp.com/collection/albums")
-        // axios.get("http://localhost:3000/collection/albums")
-        .then(response => {
-            console.log('then response.data: ', response.data);
-            console.log('then response.status: ', response.status);
-            this.setState({ apiResponse: response.data })
+    fetchAlbums() {
+        console.log('Inside fetchAlbums');
+        // axios.get("https://mymusic-backend.herokuapp.com/collection/albums");
+        axios.get('http://localhost:3000/collection/albums/')
+        .then( response => {
+            console.log('ResponseData: ', response.data);
+            console.log('ResponseStatus: ', response.status);
+            this.setState({ albumList: response.data })
         })
-        .catch(response => {
-            console.log('catch response.data: ', response.data);
-            console.log('catch response.status: ', response.status);
-        });
+        .catch( response => {
+            console.log('catch response.message: ', response.message);
+        })
     }
 
     componentDidMount() {
-        // fetch('/users')
-        // .then(res => res.json())
-        // .then(users => this.setState({ users }));
-        this.callAPI();
+        this.fetchAlbums();
     }
 
     render() {
         return(
             <div className="main-container">
-                <p className="App-intro">{this.state.apiResponse}</p>
+                <p className="App-intro">{this.state.albumList
+                ? this.state.albumList[0].title
+                : ''
+
+                }
+
+                </p>
             </div>
         );
     }
